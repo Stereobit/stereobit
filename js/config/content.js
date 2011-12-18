0040811da48d8b92247be356880be3d1f425bde9
Tobias Otte <tobias.otte@xing.com>
2011-12-19 00:01:52 +0100

    add content file

---
+++ js/config/content.js
@@ -0,0 +1,13 @@
+var sbApp = sbApp || {};
+    sbApp.config = sbApp.config || {};
+
+sbApp.config.content = {
+  "home" : { 
+    "content" : "Tobias polishes <dfn id=\"css\"><abbr title=\"Cascading Style Sheets\">CSS</abbr></dfn>, <dfn id=\"js\"><abbr title=\"JavaScript\">JS</abbr></dfn> and <dfn id=\"html\" title=\"Hypertext Markup Language\">markup</dfn>. He finds his interest piqued by the likes of accessibility, semantics, and usability. His goal is to create the most usable, fast and handsome interface that's possible.",
+    "title" : "Tobias Otte - Front-End Architect"
+  },
+  "errorPage" : { 
+    "content" : "Error 404",
+    "title" : "404 - Not found"
+  }
+};
