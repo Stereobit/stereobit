c452011bcb7fd1fb7e0cc038065e546b6a138670
Tobias Otte <t@stereob.it>
2021-01-10 13:04:26 +0000

    update packages

--- webpack.development.js
+++ webpack.development.js
@@ -3,15 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/index.js'],
+  entry: ["@babel/polyfill", "./src/index.js"],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
-    port: 3000
+    port: 3000,
   },
   watch: true,
-  devtool: 'eval-source-map',
-  mode: 'development',
+  devtool: "eval-source-map",
+  mode: "development",
   watchOptions: {
     ignored: /node_modules/,
   },
@@ -21,14 +21,15 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: [/\.css$/],
         use: [
-          'style-loader',
-          { loader: 'css-loader',
+          "style-loader",
+          {
+            loader: "css-loader",
             options: {
               url: true,
             },
@@ -38,20 +39,21 @@ module.exports = {
       {
         test: [/\.scss$/],
         use: [
-          'style-loader',
-          { loader: 'css-loader',
+          "style-loader",
+          {
+            loader: "css-loader",
             options: {
               url: true,
             },
           },
-          'sass-loader',
+          "sass-loader",
         ],
       },
       {
         test: /\.(png|jpg|gif|ttf|svg|eot|woff2|woff)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
           },
         ],
       },
@@ -59,13 +61,12 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index.html'),
-      filename: 'index.html',
-      inject: 'body',
+      template: path.join(__dirname, "src/index.html"),
+      filename: "index.html",
+      inject: "body",
     }),
-    new CopyWebpackPlugin([
-      { from: 'src/img' },
-      { from: 'src/rootfiles' },
-    ]),
-  ]
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/rootfiles" }],
+    }),
+  ],
 };
--- webpack.production.js
+++ webpack.production.js
@@ -3,101 +3,104 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/index.js'],
+  entry: ["@babel/polyfill", "./src/index.js"],
   output: {
-    filename: '[hash].[name].js',
-    sourceMapFilename: '[hash].[name].js.map',
-    publicPath: '/',
-    path: path.resolve(__dirname, 'build'),
+    filename: "[contenthash].[name].js",
+    sourceMapFilename: "[contenthash].[name].js.map",
+    publicPath: "/",
+    path: path.resolve(__dirname, "build"),
   },
-  devtool: 'source-map',
-  mode: 'production',
+  devtool: "source-map",
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
-      }, {
+      },
+      {
         test: [/\.css$/],
         use: [
-           MiniCssExtractPlugin.loader,
-          { loader: 'css-loader',
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
             options: {
-              url: true,
-            },
-          },
-          { loader: 'postcss-loader',
-            options: {
-              plugins: [
-                autoprefixer({ grid: true, browsers: ['> 5%', 'IE >= 11', 'safari >= 8'] }),
-              ],
+              postcssOptions: {
+                plugins: [
+                  autoprefixer({
+                    grid: true,
+                  }),
+                ],
+              },
             },
           },
         ],
-      }, {
+      },
+      {
         test: [/\.scss$/],
         use: [
-           MiniCssExtractPlugin.loader,
-          { loader: 'css-loader',
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
             options: {
-              url: true,
+              postcssOptions: {
+                plugins: [
+                  autoprefixer({
+                    grid: true,
+                  }),
+                ],
+              },
             },
           },
-          { loader: 'postcss-loader',
-            options: {
-              plugins: [
-                autoprefixer({ grid: true, browsers: ['> 5%', 'IE >= 11', 'safari >= 8'] }),
-              ],
-            },
-          },
-          'sass-loader',
+          "sass-loader",
         ],
-      }, {
+      },
+      {
         test: /\.(png|jpg|gif|ttf|svg|eot|woff2|woff)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
           },
         ],
       },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index.html'),
-      filename: 'index.html',
-      inject: 'body',
+    new MiniCssExtractPlugin({
+      filename: "[contenthash].[name].css",
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/404.html'),
-      filename: '404.html',
-      inject: 'body',
+      template: path.join(__dirname, "src/index.html"),
+      filename: "index.html",
+      inject: "body",
     }),
-    new CopyWebpackPlugin([
-      { from: 'src/img' },
-      { from: 'src/rootfiles' },
-    ]),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/404.html"),
+      filename: "404.html",
+      inject: "body",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/rootfiles" }],
+    }),
     new webpack.DefinePlugin({
-      'process.env': Object.assign({
-        'NODE_ENV': JSON.stringify('production'),
+      "process.env": Object.assign({
+        NODE_ENV: JSON.stringify("production"),
       }),
     }),
     new CompressionPlugin({
-      algorithm: 'gzip',
+      algorithm: "gzip",
       test: /\.(js|html)$/,
       threshold: 10240,
       minRatio: 0.8,
     }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash].css',
-      chunkFilename: '[id].[hash].css',
-    }),
-  ]
+  ],
 };
